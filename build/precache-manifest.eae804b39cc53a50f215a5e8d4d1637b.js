7460bdc3fda2e4495b57b1920abca69cabc352ef
Harry <harpreetgoraya95@gmail.com>
2020-07-16 19:48:21 -0400

    added build folder to git

---
+++ build/precache-manifest.eae804b39cc53a50f215a5e8d4d1637b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233f27055a191b9e43c7570f46d59a44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36062fa0f4e4dd28f91f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "077060f7cf12edf1ea53",
+    "url": "/static/js/2.5eda035f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5eda035f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36062fa0f4e4dd28f91f",
+    "url": "/static/js/main.6787a7f3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
